8531e5bb5a61aa501eafc1612ea494782054130d
juanluna96 <juandluna17@gmail.com>
2021-02-25 18:43:02 -0500

    Obteniendo información del artista desde otra API

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   // Definir el state
   const [busquedaLetra, setBusquedaLetra] = useState({});
   const [letra, setLetra] = useState('');
+  const [infoArtista, setInfoArtista] = useState({});
 
   useEffect(() => {
     if (Object.keys(busquedaLetra).length === 0) return;
@@ -18,10 +19,15 @@ function App() {
       const { artista, cancion } = busquedaLetra;
 
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const url_artista = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
 
-      const resultado = await axios.get(url);
+      const [letra, informacion] = await Promise.all([
+        axios.get(url),
+        axios.get(url_artista)
+      ]);
 
-      setLetra(resultado.data.lyrics);
+      setLetra(letra.data.lyrics);
+      setInfoArtista(informacion.data.artist[0]);
     };
 
     consultarAPILetra();
