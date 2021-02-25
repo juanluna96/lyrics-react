1452e259ac4dcad3b9ce8def0ca26aa0812f87d4
juanluna96 <juandluna17@gmail.com>
2021-02-25 16:44:15 -0500

    Leyendo la información en el state

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Formulario = () => {
+
+
+    const [busqueda, setBusqueda] = useState({
+        artista: '',
+        cancion: ''
+    });
+
+    const [error, setError] = useState(false);
+
+    // Funcion a cada input para leer su contenido
+
+    const actualizarState = (e) => {
+        setBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const { artista, cancion } = busqueda;
+
+    // Consultar las apis
+
+    const buscarInformacion = (e) => {
+        e.preventDefault();
+
+        if (artista.trim() === '' || cancion.trim() === '') {
+            setError(true);
+            return;
+        }
+
+        setError(false);
+
+        // Todo bien, pasar al componente principal
+    }
+
     return (
         <div className="bg-info">
             <div className="container">
                 <div className="row">
-                    <form className="pt-5 pb-2 mb-3 text-white bg-transparent col card">
+                    <form className="pt-5 pb-2 mb-3 text-white bg-transparent col card" onSubmit={ buscarInformacion }>
                         <fieldset>
                             <legend className="text-center font-weight-bold text-uppercase">Buscador letras Canciones</legend>
 
@@ -13,13 +48,13 @@ const Formulario = () => {
                                 <div className="col-md-6">
                                     <div className="form-group">
                                         <label htmlFor="artista" className="font-italic">Artista:</label>
-                                        <input id="artista" className="form-control" type="text" name="artista" />
+                                        <input id="artista" className="form-control" type="text" name="artista" value={ artista } onChange={ actualizarState } />
                                     </div>
                                 </div>
                                 <div className="col-md-6">
                                     <div className="form-group">
                                         <label htmlFor="cancion" className="font-italic">Canción:</label>
-                                        <input id="cancion" className="form-control" type="text" name="cancion" />
+                                        <input id="cancion" className="form-control" type="text" name="cancion" value={ cancion } onChange={ actualizarState } />
                                     </div>
                                 </div>
                             </div>
